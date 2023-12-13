b171eef81c6eff79f057ec7677db778d176e5511
Nikita Smirnov <cnism001@edu.xamk.fi>
2023-12-13 18:54:49 +0200

    added VeganRamenBuilder.cs

---
+++ BuilderAssignment/Builders/VeganRamenBuilder.cs
@@ -0,0 +1,42 @@
+﻿using BuilderAssignment.Models;
+using System.Collections.Generic;
+
+namespace BuilderAssignment.Builders
+{
+    public class VeganRamenBuilder : IRamenBuilder
+    {
+        private RamenBowl ramen = new RamenBowl();
+
+        public IRamenBuilder SetBroth(BrothType broth)
+        {
+            ramen.Broth = BrothType.Vegan; 
+            return this;
+        }
+
+        public IRamenBuilder SetNoodles(NoodleType noodles)
+        {
+            ramen.Noodles = NoodleType.Soba;
+            return this;
+        }
+
+        public IRamenBuilder AddToppings(Topping topping)
+        {
+            // Add toppings to the list
+            ramen.Toppings.Add(Topping.BambooShoots); 
+            ramen.Toppings.Add(Topping.Seaweed);
+            return this;
+        }
+
+        public IRamenBuilder AddExtras(Extra extra)
+        {
+            // Assuming no extras are added for Vegan Ramen
+           
+            return this;
+        }
+
+        public RamenBowl GetRamen()
+        {
+            return ramen;
+        }
+    }
+}
